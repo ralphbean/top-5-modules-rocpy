565d002c2057c6d68a84a29d12d2c577dfd972f4
Ralph Bean <rbean@redhat.com>
2013-01-15 13:22:27 -0500

    Notes for example 7.

---
+++ src/sqlalchemy/example7.py
@@ -0,0 +1,12 @@
+# advanced uses
+#
+#   introspection       (tw2.jit)
+#       https://github.com/toscawidgets/tw2.jit/blob/develop/tw2/jit/widgets/sqla.py#L146
+#
+#   reflection          (slurchemy)
+#       https://github.com/ralphbean/slurchemy/blob/master/slurchemy/reflect.py#L95
+#
+#   session extensions  (http://apply.rc.rit.edu)
+#       (unfortunately, not open-sourced..)
+#       - versioned extension
+#       - trac extension
