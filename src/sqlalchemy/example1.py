7db89428964f6b90b8a2f961708a01bb8d8622c6
Ralph Bean <rbean@redhat.com>
2013-01-15 11:21:27 -0500

    Start of sqlalchemy examples.

---
+++ src/sqlalchemy/example1.py
@@ -0,0 +1,24 @@
+
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    fullname = Column(String)
+    password = Column(String)
+
+    def __init__(self, name, fullname, password):
+        self.name = name
+        self.fullname = fullname
+        self.password = password
+
+    def __repr__(self):
+        return "<User('%s','%s', '%s')>" % (
+            self.name, self.fullname, self.password)
